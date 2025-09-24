87c6e9f649ce544d558ae124c16d446d3d652c39
big dawg gwood <101984762+redfrog3@users.noreply.github.com>
2025-09-24 16:19:01 +0000

    Class code work: sep 24

--- main.py
+++ main.py
@@ -1,3 +1,35 @@
 print("Hello world")
 print("Age: ", 24)
 print("sum: ", 4 + 5)
+# This is a comment
+#DATA TYPES
+
+# 1. STRINGS:
+print("Hello world")
+print("once upon a time \na cow fell off a cliff. \nffilc a ffo llef woc emit a nopu ecno")
+print("name: \tbilly \nage: \t14 \nGrade: /t9th")
+# INTEGERS
+
+print(42)
+print(-23)
+print(5 + 5 - 11) #comment
+
+# FLOAT
+print(4.0)
+print(2.0 + 4.5)
+print(2.0 + 4)
+
+#BOOLEAN
+print(True) #1
+print(False) #0
+print(1 == 1)
+print(1 == 2)
+print(1 != 2)
+print(1 != 1)
+print("hello" == "hello")
+print("Hello" == "hello")
+print("Hello" != "goodby")
+print(5 + 5 != 11)
+
+type(type("To see this text, you have to wrap me in a print statement"))
+print(type(3))

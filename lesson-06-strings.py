5c5ae9d9c23390ff6c1d1b03bf769254fa788e87
big dawg gwood <101984762+redfrog3@users.noreply.github.com>
2025-10-08 16:54:07 +0000

    oct 8 2025 classwork strings

--- lesson-06-strings.py
+++ lesson-06-strings.py
@@ -24,6 +24,11 @@ print("reversed, Steping through every every letter:", word[::-3])
 phrase = input("Enter a phrase")
 print("Here is every third letter of your phrase: ",  phrase [::-3])
 
+## Built-in string functions
+
+last_name = "Lovelace"
+length = len(last_name)
+print("Length of name:", length)
 
 ## Built in functions
 
@@ -33,3 +38,32 @@ print(length_of_word)
 statement = "PaTrIcK"
 print("\nUppercase:", statement.upper())
 print("\nLowercase:  ", statement.lower())
+
+
+sentace = "your a goofy goober"
+new_sentace = sentace.replace("your", "I'm")
+
+#Find and replace text
+sentence = "I'm a goofy goober"
+new_sentence = sentence.replace("I'm", "You're")
+next_replacement = sentence.replace("goofy", "goober")
+print(sentence)
+print(new_sentence)
+print(next_replacement)
+
+
+# FORMATTED STRINGS IN PYTHON
+# f-strings allow variables and expressions inside strings
+
+print("\n--- Formatted Strings ---")
+
+name = "Big dawg da absalut G"
+age = 5
+city = "dubai"
+
+print(f"Hello, my name is {name}.\nI am {age} years old \nand I live in {city}.")
+
+
+# f-strings can do math and function calls inside {}
+
+print(f"Next year, i,ll be {age + 1}. My name in uppercase is {name.upper()}")

a529c22351f1b4a93269190caacb3a1cf3402394
big dawg gwood <101984762+redfrog3@users.noreply.github.com>
2025-10-10 12:46:32 +0000

    oct 9 madlibs

---
+++ madlibs.py
@@ -0,0 +1,29 @@
+
+# 4 nouns  # 3 adjectives  #3 verbs
+
+# worked with nathan///
+
+
+#NOUNS
+name = input("Enter a Name: ") #CAPITILIZE DONE
+noun_2 = input("Enter a noun: ") #DONE
+noun_3 = input("Enter a place: ") #CAPITILIZE
+noun_4 = input("Enter a famous person: ") #DONE
+noun_5 = input("Enter a amount of money")
+money_amount = int(noun_5)
+
+
+#ADJECTIVES
+adjective_1 = input("Enter a adjective: ") #DONE
+adjective_2 = input("Enter another adjective: ") #DONE
+adjective_3 = input("Enter one more majestic adjective: ")
+
+#VERBS
+verb_1 = input("Enter a destructive verb that ends in ing: ") #DONE
+verb_2 = input("Enter another verb: ") #DONE
+verb_3 = input("Enter one more verb that you would relate with movement: ")#DONE
+
+
+
+
+print(f"\tThis is the story of {name.capitalize()} and how he crashed his car into {noun_4.capitalize()}. \n During his first day at {adjective_1.capitalize()} {noun_2.capitalize()} INC, he was was assigned a buisiness trip to {noun_3.capitalize()}. He was on his way to the airpot and \nthen while {verb_1} a {adjective_2} chicken alfrado. {name.capitalize()} was so into {verb_1} his {adjective_2} chicken alfrado that his foot {verb_2} the wheel and the\n car {verb_3} into the famous person, {noun_4.capitalize()}. {noun_4.capitalize()} flew up into the air {adjective_3} and hit the ground. He looked horrid and \nwas bent over like Peter Griffan. {noun_4.capitalize()} later then sued {name.capitalize()} for {noun_5}.")
